c2bb0d19d3b199530ddd3e2b1338df3b822bb116
wasihun-code <wasihunageru7947@gmail.com>
2023-02-07 12:05:31 +0530

    Changed date id to dob id  due to test specifications

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 const submitBtn = document.getElementById('submit');
 const email = document.getElementById('email');
-const dob = document.getElementById('date')
+const dob = document.getElementById('dob')
 
 email.addEventListener('input', () => validateEmail(email));
 submitBtn.addEventListener('click', (event) => {
     const name = document.getElementById('name');
     const password = document.getElementById('password');
-    const dob = document.getElementById('date');
+    const dob = document.getElementById('dob');
     const acceptedTerms = document.getElementById('acceptTerms')
     validateEmail(email);
     if (name.checkValidity() && email.checkValidity() 
@@ -35,13 +35,13 @@ function saveToLocalStorage() {
     const nameValue = document.getElementById('name').value;
     const emailValue = document.getElementById('email').value;
     const passwordValue = document.getElementById('password').value;
-    const dobValue = document.getElementById('date').value;
+    const dobValue = document.getElementById('dob').value;
     let acceptedValue = document.getElementById('acceptTerms').checked;
 
         localStorage.setItem('name', nameValue);
         localStorage.setItem('email', emailValue);
         localStorage.setItem('password', passwordValue);
-        localStorage.setItem('date', dobValue);
+        localStorage.setItem('dob', dobValue);
         localStorage.setItem('acceptedTerms', acceptedValue)
 }
 
@@ -49,7 +49,7 @@ function appendLocalStorageToTable() {
     const nameValue = localStorage.getItem('name');
     const emailValue = localStorage.getItem('email');
     const passwordValue = localStorage.getItem('password');
-    const dobValue = localStorage.getItem('date');
+    const dobValue = localStorage.getItem('dob');
     const acceptedValue = localStorage.getItem('acceptedTerms');
 
 
